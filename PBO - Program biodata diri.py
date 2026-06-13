6eb1ea1a44644c0d2515331dd1571f15c050141b
Heppy <heppyzhuhri01@gmail.com>
2026-06-13 20:37:51 +0700

    Add files via upload

---
+++ PBO - Program biodata diri.py
@@ -0,0 +1,29 @@
+#Latihan Pyhton Sederhana
+#HeppyListianiZhuhri
+
+print('PROGRAM BIODATA DIRI')
+print('=====================')
+
+Nama= input('Nama : ')
+Umur= input('Umur : ')
+Alamat= input('Alamat : ')
+Pekerjaan= input('Pekerjaan : ')
+Status= input('Status : ')
+Hobi= input('Hobi : ')
+Jurusan= input('Jurusan : ')
+Jeniskelamin= input('Jenis Kelamin : ')
+TempatLahir= input('Tempat Lahir : ')
+TanggalLahir= input('Tanggal Lahir : ')
+print("\n ")
+
+print('=====================')
+print ("Nama Saya Adalah " + Nama)
+print ("Umur Saya Adalah %d Tahun" % int(Umur))
+print ("Alamat Saya Berada Di " + Alamat)
+print ("Pekerjaan Saya Adalah " + Pekerjaan)
+print ("Status Saya Adalah " + Status)
+print ("Hobi Saya Adalah " + Hobi)
+print ("Jurusan Saya Adalah " + Jurusan)
+print ("Jenis Kelamin Saya Adalah " + Jeniskelamin)
+print ("Tempat Lahir Saya Adalah " + TempatLahir)
+print ("Tanggal Lahir Saya Adalah " + TanggalLahir)
